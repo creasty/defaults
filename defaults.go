edb48d05d216b895f0b46bd8c2d432d20c3a4f25
licaonfee <jmgr@outlook.cl>
2020-07-22 21:03:27 -0400

    remove redundant constants

--- defaults.go
+++ defaults.go
@@ -14,13 +14,6 @@ var (
 
 const (
 	fieldName = "default"
-
-	autoBase = 0
-
-	bitSize8  = 8
-	bitSize16 = 16
-	bitSize32 = 32
-	bitSize64 = 64
 )
 
 // Set initializes members in a struct referenced by a pointer.
@@ -65,57 +58,57 @@ func setField(field reflect.Value, defaultVal string) error {
 				field.Set(reflect.ValueOf(val).Convert(field.Type()))
 			}
 		case reflect.Int:
-			if val, err := strconv.ParseInt(defaultVal, autoBase, strconv.IntSize); err == nil {
+			if val, err := strconv.ParseInt(defaultVal, 0, strconv.IntSize); err == nil {
 				field.Set(reflect.ValueOf(int(val)).Convert(field.Type()))
 			}
 		case reflect.Int8:
-			if val, err := strconv.ParseInt(defaultVal, autoBase, bitSize8); err == nil {
+			if val, err := strconv.ParseInt(defaultVal, 0, 8); err == nil {
 				field.Set(reflect.ValueOf(int8(val)).Convert(field.Type()))
 			}
 		case reflect.Int16:
-			if val, err := strconv.ParseInt(defaultVal, autoBase, bitSize16); err == nil {
+			if val, err := strconv.ParseInt(defaultVal, 0, 16); err == nil {
 				field.Set(reflect.ValueOf(int16(val)).Convert(field.Type()))
 			}
 		case reflect.Int32:
-			if val, err := strconv.ParseInt(defaultVal, autoBase, bitSize32); err == nil {
+			if val, err := strconv.ParseInt(defaultVal, 0, 32); err == nil {
 				field.Set(reflect.ValueOf(int32(val)).Convert(field.Type()))
 			}
 		case reflect.Int64:
 			if val, err := time.ParseDuration(defaultVal); err == nil {
 				field.Set(reflect.ValueOf(val).Convert(field.Type()))
-			} else if val, err := strconv.ParseInt(defaultVal, autoBase, bitSize64); err == nil {
+			} else if val, err := strconv.ParseInt(defaultVal, 0, 64); err == nil {
 				field.Set(reflect.ValueOf(val).Convert(field.Type()))
 			}
 		case reflect.Uint:
-			if val, err := strconv.ParseUint(defaultVal, autoBase, strconv.IntSize); err == nil {
+			if val, err := strconv.ParseUint(defaultVal, 0, strconv.IntSize); err == nil {
 				field.Set(reflect.ValueOf(uint(val)).Convert(field.Type()))
 			}
 		case reflect.Uint8:
-			if val, err := strconv.ParseUint(defaultVal, autoBase, bitSize8); err == nil {
+			if val, err := strconv.ParseUint(defaultVal, 0, 8); err == nil {
 				field.Set(reflect.ValueOf(uint8(val)).Convert(field.Type()))
 			}
 		case reflect.Uint16:
-			if val, err := strconv.ParseUint(defaultVal, autoBase, bitSize16); err == nil {
+			if val, err := strconv.ParseUint(defaultVal, 0, 16); err == nil {
 				field.Set(reflect.ValueOf(uint16(val)).Convert(field.Type()))
 			}
 		case reflect.Uint32:
-			if val, err := strconv.ParseUint(defaultVal, autoBase, bitSize32); err == nil {
+			if val, err := strconv.ParseUint(defaultVal, 0, 32); err == nil {
 				field.Set(reflect.ValueOf(uint32(val)).Convert(field.Type()))
 			}
 		case reflect.Uint64:
-			if val, err := strconv.ParseUint(defaultVal, autoBase, bitSize64); err == nil {
+			if val, err := strconv.ParseUint(defaultVal, 0, 64); err == nil {
 				field.Set(reflect.ValueOf(val).Convert(field.Type()))
 			}
 		case reflect.Uintptr:
-			if val, err := strconv.ParseUint(defaultVal, autoBase, strconv.IntSize); err == nil {
+			if val, err := strconv.ParseUint(defaultVal, 0, strconv.IntSize); err == nil {
 				field.Set(reflect.ValueOf(uintptr(val)).Convert(field.Type()))
 			}
 		case reflect.Float32:
-			if val, err := strconv.ParseFloat(defaultVal, bitSize32); err == nil {
+			if val, err := strconv.ParseFloat(defaultVal, 32); err == nil {
 				field.Set(reflect.ValueOf(float32(val)).Convert(field.Type()))
 			}
 		case reflect.Float64:
-			if val, err := strconv.ParseFloat(defaultVal, bitSize64); err == nil {
+			if val, err := strconv.ParseFloat(defaultVal, 64); err == nil {
 				field.Set(reflect.ValueOf(val).Convert(field.Type()))
 			}
 		case reflect.String:
